b85a9a489bdfbb965bf3b2ca702dd304f92e08c6
Unknown <rbeckstead_1991@hotmail.com>
2018-07-18 15:16:42 -0600

    added router

---
+++ samp/src/Landing.js
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+import firebase, {auth,provider} from './firebase.js';
+class Landing  extends Component {
+    render() {
+
+    }
+}
+export default Landing;
